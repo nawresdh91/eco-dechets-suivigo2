6c9ed7cb6e2dc28010d0ebe37e027d17fa53e043
Scoty Loumbou <scott@artkodes.com>
2022-12-02 09:57:14 +0100

    heroku config

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"{scoty.loumbou@eco-dechets.fr}\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\port = $PORT\n\
+" > ~/.streamlit/config.toml
